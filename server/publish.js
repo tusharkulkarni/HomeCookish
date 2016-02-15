5ceddac870a35cfc387dfba05f20f7e70cd9bd5a
tusharkulkarni <tusharkulkarni1990@gmail.com>
2016-02-15 03:52:49 -0500

    Made changes to Fetch those dishes which are selected to be in menu

--- server/publish.js
+++ server/publish.js
@@ -1,17 +1,21 @@
 Meteor.publish('recipes', function(lat, lon){
 	console.log("latitude : " + lat);
 
-	if(lat){
-		console.log("inside if");
-	return Recipes.find( { location :
-                         		{ $near :
-                           			{ $geometry :
-                              			{ type : "Point" ,
-                                		coordinates : [lat, lon] 
-                                		} ,
+	if(lat){		
+
+	
+	return Recipes.find( { $and: [ { inMenu: true },
+								{ location :
+                         			{ $near :
+                           				{ $geometry :
+                              				{ type : "Point" ,
+                                			coordinates : [lat, lon] 
+                                			} ,
                              			$maxDistance : 10000
+                         				} 
                          			} 
-                         		} 
+                         		}
+                         		]
                         	} );
 	}else{
 	return Recipes.find({});
